7d3d5c47c0321337af551e4af88a5fe67d504002
JulioCzar <cesarguzma@gmail.com>
2016-06-14 03:12:47 +0000

    Sin rails

--- indices.rb
+++ indices.rb
@@ -10,7 +10,7 @@ class Indices
     struct = Structures.new()
   
     #spi.spiders
-<<<<<<< HEAD
+
     hash = norm.normalize_text(File.read("texto.txt"))
     
     struct.add_document("texto.txt", hash)
@@ -19,13 +19,13 @@ class Indices
     
     struct.add_document("texto2.txt", hash)
     
-=======
+
     hash = norm.normalize_text(File.read("sinRepetidosTitulos.txt"))
     hash.each do |word, count|
         sub_indices = ind.union_hash(indice_terminos[word], {id=>count})
         indice_terminos[word] = sub_indices
     end
->>>>>>> 88ba56eeee1234d3a013e373d788da91b1c1b9d7
+
 
     puts struct.get_all_terms_counts()
     puts struct.get_all_terms_tf_idf()
