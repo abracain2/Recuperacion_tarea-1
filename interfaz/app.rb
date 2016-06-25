fed3a2dc904ed15408db90f7980f2e6db043969b
abraham <abracaineveg@gmail.com>
2016-06-25 21:33:54 +0000

    se agrego la funcionalidad de los sinonimos, tambien el bundle y la configuracion del mismo, recordar estar vigilando el key de la api de sinonimos

--- interfaz/app.rb
+++ interfaz/app.rb
@@ -1,6 +1,7 @@
 # myapp.rb
 require 'rubygems'
 require 'sinatra'
+require "dinosaurus"
 require_relative '../indices'  
 require_relative '../packages/structures'
 require_relative '../packages/normalize'
@@ -20,20 +21,36 @@ end
 
 post '/' do
     
+    Dinosaurus.configure do |config|
+      config.api_key = 'ddec603cf16e46327f57f1ce15dfcbe6'
+    end
+    
     structtemp = Hash.new()
     
     consulta = "#{params['buscar']}"
     
     consulta = norm.normalize_text_consult(consulta)
     
+    sugerencias = Array.new
+    
     consulta.each do |word|
+        
+        Dinosaurus.synonyms_of(word).each do |sug|
+            sugerencias.push(sug)
+        end
     
         structtemp = struct.union_avg_hash(structtemp, struct.term_tf_idf_in_documents(word))
         
     end
     
+    result = Hash.new()
     
-    structtemp = structtemp.sort_by { |id, tf_idf| tf_idf }.reverse!
+    result['resultados'] = structtemp.sort_by { |id, tf_idf| tf_idf }.reverse!
+    
+    sugerencias = sugerencias.uniq
+    
+    result['sugerencias'] = sugerencias
+    
     
     #if structtemp != 0
         #structtemp.each do |id, tf_idf|
@@ -45,5 +62,9 @@ post '/' do
         #end
     #end
     
-    return structtemp.inspect
+    puts structtemp
+    
+    puts sugerencias
+    
+    return result.inspect
 end
---
+++ interfaz/config/initializers/dinosaurus.rb
@@ -0,0 +1,5 @@
+# initializer of synonyms api
+
+Dinosaurus.configure do |config|
+  config.api_key = 'ddec603cf16e46327f57f1ce15dfcbe6'
+end
